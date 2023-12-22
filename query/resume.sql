c8bbd664a66c620435c67bb1e17a73a7beff3e0c
nghia pham <phamhongnghia13579@gmail.com>
2023-12-22 21:17:39 +0700

    myapp - update

--- query/resume.sql
+++ query/resume.sql
@@ -4,7 +4,7 @@ LIMIT 0, 1000
 
 -- Date: 2022-09-03 23:57
 */
-INSERT INTO myresume_resume  (id,name,birthday,phone,address,email,story,position,image,work_exp,part_project,gpa,university,linked) VALUES (1,'Nghia Pham','1999-08-10','0356156445','Mo Duc, Quang Ngai, Viet Nam','nghiaphamhong99@gmail.com','Hello everyone, I was born and raised in Quang Ngai province, I studied at Pham Van Dong high school and a university of the same name in my hometown Quang Ngai. I graduated from university in June of 2021. I have 2 years experience in Java developer at TMA solution Binh Dinh','Java Developer','resume-images/DSC_63912Tam_9x12.JPG',2,2,3.15,'Pham Van Dong','https://www.linkedin.com/in/nghia-pham-a641621b1');
+INSERT INTO myresume_resume  (id,name,birthday,phone,address,email,story,position,image,work_exp,part_project,gpa,university,linked) VALUES (1,'Nghia Pham','1999-08-10','0356156445','Mo Duc, Quang Ngai, Viet Nam','nghiaphamhong99@gmail.com','Hello everyone, I was born and raised in Quang Ngai province, I studied at Pham Van Dong high school and a university of the same name in my hometown Quang Ngai. I graduated from university in June of 2021. I have 2 years experience in Java developer at TMA solution Binh Dinh','Software Engineer','resume-images/avatar_4huorvJ.jpg',2.7,2,3.15,'Pham Van Dong','https://www.linkedin.com/in/nghia-pham-a641621b1');
 
 
 
@@ -40,7 +40,7 @@ LIMIT 0, 1000
 
 -- Date: 2022-09-03 23:59
 */
-INSERT INTO myresume_companiesworked (id,company_exp,company_name_id,name_id) VALUES (2,1,3,1);
+INSERT INTO myresume_companiesworked (id,company_exp,company_name_id,name_id) VALUES (2,2.7,3,1);
 
 
 
@@ -51,4 +51,4 @@ LIMIT 0, 1000
 -- Date: 2022-09-03 23:58
 */
 INSERT INTO myresume_project (id,project_name,project_des,project_image,name_id) VALUES (1,'Nokia','Device & Network','resume-image/nokia.png',1);
-INSERT INTO myresume_project (id,project_name,project_des,project_image,name_id) VALUES (2,'Manage Employee','Internship project','resume-image/manage-staff.png',1);
+INSERT INTO myresume_project (id,project_name,project_des,project_image,name_id) VALUES (2,'Internship project','Employee Management','resume-image/manage-staff.png',1);
